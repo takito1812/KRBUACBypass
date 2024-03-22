c2e66617d61796516b927281036f008405c45d68
Víctor García <56491288+takito1812@users.noreply.github.com>
2024-03-22 10:13:55 +0100

    Update Program.cs

--- KRBUACBypass/Program.cs
+++ KRBUACBypass/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net;
 using CommandLine;
 using CommandLine.Text;
@@ -9,8 +9,11 @@ namespace KRBUACBypass
 {
     public class Options
     {
+        [Option('c', "Command", Required = false, HelpText = "Program to run.")]
+        public string Command { get; set; }
+
         [Option('v', "Verbose", Required = false, HelpText = "Output verbose debug information.")]
-        public bool Verbose { get; set; } = true;
+        public bool Verbose { get; set; }
     }
 
     internal class Program
@@ -21,11 +24,11 @@ namespace KRBUACBypass
         public static bool BogusMachineID = true;
         static void Main(string[] args)
         {
-            var ParserResult = new Parser(with => with.HelpWriter = null)
+            var ParserResult = new CommandLine.Parser(with => with.HelpWriter = null)
                 .ParseArguments<Options>(args);
-            
+
             ParserResult
-                .WithParsed(options => Run(options))
+                .WithParsed(options => Run(args, options))
                 .WithNotParsed(errs => DisplayHelp(ParserResult));
         }
 
@@ -35,8 +38,8 @@ namespace KRBUACBypass
             {
                 h.AdditionalNewLineAfterOption = false;
                 h.MaximumDisplayWidth = 100;
-                h.Heading = "\ntstaklbgl2m 1.0.0-beta"; //change header
-                h.Copyright = "Copyright (c) 2024"; //change copyright text
+                h.Heading = "\nKRBUACBypass 1.0.0-beta"; //change header
+                h.Copyright = "Copyright (c) 2023"; //change copyright text
                 return HelpText.DefaultParsingErrorsHandler(result, h);
             }, e => e);
             Console.WriteLine(helpText);
@@ -44,6 +47,7 @@ namespace KRBUACBypass
 
         private static void Run(string[] args, Options options)
         {
+            string command = options.Command;
             Verbose = options.Verbose;
 
             string domainController = Networking.GetDCName();
