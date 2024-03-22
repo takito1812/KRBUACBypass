030e8f6bf36301a94d57d52e61c871abc931345f
Víctor García <56491288+takito1812@users.noreply.github.com>
2024-03-22 10:26:59 +0100

    Update Program.cs

--- KRBUACBypass/Program.cs
+++ KRBUACBypass/Program.cs
@@ -1,13 +1,16 @@
-using System;
+﻿using System;
 using System.Net;
 using KRBUACBypass.lib.Interop;
+using System.Collections.Generic;
 
 namespace KRBUACBypass
 {
     internal class Program
     {
         public static bool wrapTickets = false;
+        public static bool Debug = false;
         public static bool Verbose = true;
+        public static bool BogusMachineID = true;
 
         static void Main(string[] args)
         {
@@ -19,6 +22,8 @@ namespace KRBUACBypass
             string domainController = Networking.GetDCName();
             string service = $"HOST/{Dns.GetHostName()}";
             Interop.KERB_ETYPE requestEType = Interop.KERB_ETYPE.subkey_keymaterial;
+            string outfile = "";
+            bool ptt = false;
 
             byte[] blah = LSA.RequestFakeDelegTicket();
         }
